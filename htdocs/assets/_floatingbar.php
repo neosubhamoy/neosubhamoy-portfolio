33231c2bf3215e43c1ef6519c2f1ea1a4fc86fec
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2023-11-05 14:21:04 +0530

    (refactor): created separate component of prloader, floatingbar in assets

---
+++ htdocs/assets/_floatingbar.php
@@ -0,0 +1,7 @@
+<div id="floating-bar" class="fixed inset-x-0 bottom-[7vh] z-[50] flex justify-center items-center">
+    <div id="searchbar" class="searchbar bg-bg_secondary rounded-full p-[0.30rem] flex items-center cursor-pointer mx-2 hover:shadow-[0px_0px_30px] hover:shadow-accent_primary_extratransparent transition transform duration-300">
+        <button class="bg-accent_primary px-3 py-2 rounded-full"><i class="fa-solid fa-magnifying-glass text-bg_primary"></i></button>
+        <p class="ml-3 mr-4 text-accent_three">SEARCH</p>
+    </div>
+    <button id="sharebutton" class="sharebutton bg-accent_primary px-[0.80rem] py-2 rounded-full mx-2 hover:shadow-[0px_0px_30px] hover:shadow-accent_primary_transparent transition transform duration-300"><i class="fa-solid fa-share-nodes text-bg_primary"></i></button>
+</div>
---
+++ htdocs/assets/_preloader.php
@@ -0,0 +1,1 @@
+<div id="preloader" class="absolute top-0 right-0 h-full w-screen z-[100] flex justify-center items-center bg-bg_primary overflow-hidden"><div class="spinner border-[5px] border-accent_secondary_transparent border-t-[5px] border-t-accent_primary rounded-[50%] h-[50px] w-[50px] animate-spin"></div></div>
