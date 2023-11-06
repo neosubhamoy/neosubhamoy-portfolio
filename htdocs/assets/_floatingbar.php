211d40affc3d97537c33dd4a6d1092ef8d884318
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2023-11-06 20:15:45 +0530

    (feat): improved serchbar open, close animation and added blurry search background (refactor): updated flotingbar config

--- htdocs/assets/_floatingbar.php
+++ htdocs/assets/_floatingbar.php
@@ -1,7 +1,9 @@
+<div class="floatingwindowwrapper fixed top-0 left-0 w-screen h-screen z-30 bg-[rgba(0,_0,_0,_0.4)] backdrop-filter backdrop-blur-lg"></div>
 <div id="floating-bar" class="fixed inset-x-0 bottom-[7vh] z-[50] flex justify-center items-center">
     <div id="searchbar" class="searchbar bg-bg_secondary rounded-full p-[0.30rem] flex items-center cursor-pointer mx-2 hover:shadow-[0px_0px_30px] hover:shadow-accent_primary_extratransparent transition transform duration-300">
         <button class="bg-accent_primary px-3 py-2 rounded-full"><i class="fa-solid fa-magnifying-glass text-bg_primary"></i></button>
-        <p class="ml-3 mr-4 text-accent_three">SEARCH</p>
+        <p id="searchtext" class="ml-3 mr-4 text-accent_three">Search</p>
+        <input id="searchinput" class="w-full mx-2 bg-bg_secondary caret-accent_primary outline-none hidden" type="text" placeholder="Type to Search Something . . ." name="search-input">
     </div>
     <button id="sharebutton" class="sharebutton bg-accent_primary px-[0.80rem] py-2 rounded-full mx-2 hover:shadow-[0px_0px_30px] hover:shadow-accent_primary_transparent transition transform duration-300"><i class="fa-solid fa-share-nodes text-bg_primary"></i></button>
     <button id="closebutton" class="closebutton bg-accent_primary px-[0.80rem] py-2 rounded-full mx-2 hover:shadow-[0px_0px_30px] hover:shadow-accent_primary_transparent transition transform duration-300 hidden"><i class="fa-solid fa-xmark text-bg_primary"></i></button>
--- htdocs/assets/js/floatingbar-config.js
+++ htdocs/assets/js/floatingbar-config.js
@@ -3,6 +3,8 @@ const floatingBar = document.getElementById("floating-bar");
 const searchBar = document.getElementById("searchbar");
 const shareBtn = document.getElementById("sharebutton");
 const closeBtn = document.getElementById("closebutton");
+const searchTxt = document.getElementById("searchtext");
+const searchInput = document.getElementById("searchinput");
 let lastScrollTop = 0;
 
 window.addEventListener("scroll", function () {
@@ -24,46 +26,53 @@ window.addEventListener("scroll", function () {
 });
 
 // function to open floating search window
-function activate_search(searchBar, floatingBar, closeBtn, shareBtn) {
+function activate_search(searchBar, floatingBar, closeBtn, shareBtn, searchTxt, searchInput) {
     floatingBar.classList.remove("floatingbar-click-slide-down");
     floatingBar.classList.add("floatingbar-click-slide-up");
     searchBar.classList.remove("searchbar-click-decrease-width");
     searchBar.classList.add("searchbar-click-increase-width");
     closeBtn.classList.remove("hidden");
     shareBtn.classList.add("hidden");
+    searchTxt.classList.add("hidden");
+    searchInput.classList.remove("hidden");
+    searchInput.focus();
+    document.body.classList.add("overflow-hidden");
 }
 
 // function to close floating search window
-function close_search(searchBar, floatingBar, closeBtn, shareBtn) {
+function close_search(searchBar, floatingBar, closeBtn, shareBtn, searchTxt, searchInput) {
     floatingBar.classList.remove("floatingbar-click-slide-up");
     floatingBar.classList.add("floatingbar-click-slide-down");
     searchBar.classList.remove("searchbar-click-increase-width");
     searchBar.classList.add("searchbar-click-decrease-width");
     closeBtn.classList.add("hidden");
     shareBtn.classList.remove("hidden");
+    searchTxt.classList.remove("hidden");
+    searchInput.classList.add("hidden");
+    document.body.classList.remove("overflow-hidden");
 }
 
 // when the search icon is clicked
 searchBar.addEventListener("click", function () {
-    activate_search(searchBar, floatingBar, closeBtn, shareBtn);
+    activate_search(searchBar, floatingBar, closeBtn, shareBtn, searchTxt, searchInput);
 });
 
 // when ALT + K shortcut key is pressed
 document.addEventListener("keydown", function(event) {
     if (event.altKey && event.key === "k") {
-        activate_search(searchBar, floatingBar, closeBtn, shareBtn);
+        activate_search(searchBar, floatingBar, closeBtn, shareBtn, searchTxt, searchInput);
     }
 });
 
 // when close button is clicked
 closeBtn.addEventListener("click", function () {
-    close_search(searchBar, floatingBar, closeBtn, shareBtn);
+    close_search(searchBar, floatingBar, closeBtn, shareBtn, searchTxt, searchInput);
 });
 
 // when ESC key is pressed
 document.addEventListener("keydown", function(event) {
     if (event.key === "Escape") {
-        close_search(searchBar, floatingBar, closeBtn, shareBtn);
+        close_search(searchBar, floatingBar, closeBtn, shareBtn, searchTxt, searchInput);
     }
 });
 
