6ab0cecc3e0006a55b56a8acff986312ee34f939
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2023-12-04 21:46:42 +0530

    (fixed): blog page preloader and floatingbar misplacement

--- htdocs/blog.php
+++ htdocs/blog.php
@@ -11,11 +11,11 @@ require 'core/query_functions.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <title><?php echo fetch_page_title($conn, "Blog");?></title>
-    <?php include 'assets/_preloader.php';?>
-    <?php include 'assets/_floatingbar.php';?>
     <?php require 'assets/_integrate.php';?>
 </head>
 <body class="font-lexend bg-bg_primary text-accent_secondary">
+    <?php include 'assets/_preloader.php';?>
+    <?php include 'assets/_floatingbar.php';?>
     <?php require 'assets/_header.php';?>
     <div class="blogbox w-full px-4 lg:px-[4.5rem] flex flex-col items-center text-center">
         <div class="mt-[30vh] mb-[60vh]">
