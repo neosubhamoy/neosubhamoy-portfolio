c344a16afaecc572178bcb6f38828e4af8bf5678
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2023-12-05 23:55:19 +0530

    (refactor): updated contact pagetitle text

--- htdocs/contact.php
+++ htdocs/contact.php
@@ -22,8 +22,8 @@ require 'core/query_functions.php';
             <div class="herotitle w-full lg:w-auto px-2 lg:px-0 lg:mt-16 lg:mb-10 2xl:mt-24 2xl:mb-32">
                 <p class="text-xl lg:text-2xl my-3 lg:my-2">Contact Me 🛸</p>
                 <h1 class="text-2xl lg:text-3xl mt-1 mb-4 lg:mt-2 lg:mb-3 lg:whitespace-nowrap font-bold">Ring, <span class="text-accent_primary">Ring!</span> let's <span class="text-accent_primary">Talk</span></h1>
-                <h2 class="text-lg mt-3 mb-1 lg:mt-2 lg:mb-1 lg:whitespace-nowrap">You'r just one <span class="text-accent_primary font-bold">message</span> away from me</h2>
-                <h3 class="text-lg mt-1 mb-3 lg:mt-1 lg:mb-2 lg:whitespace-nowrap">I'd love to <span class="text-accent_primary font-bold">hear from you</span> just drop a <span class="text-accent_primary font-bold">line</span></h3>
+                <h2 class="text-lg mt-3 mb-1 lg:mt-2 lg:mb-1 lg:whitespace-nowrap">You're just one <span class="text-accent_primary font-bold">step</span> away from reaching out!</h2>
+                <h3 class="text-lg mt-1 mb-3 lg:mt-1 lg:mb-2 lg:whitespace-nowrap">I'd love to <span class="text-accent_primary font-bold">hear from you</span> just drop a <span class="text-accent_primary font-bold">message</span></h3>
                 <div class="w-full flex justify-start items-center my-7 lg:my-6">
                     <button class="w-fit px-5 py-1 my-2 mr-2 font-bold bg-accent_primary text-bg_primary rounded-full hover:shadow-lg hover:shadow-accent_secondary_transparent hover:bg-accent_secondary hover:rounded-lg transition transform duration-500" onclick="window.open('', '_blank')"><i class="text-bg_primary fa-brands fa-whatsapp"></i> WhatsApp</button>
                     <button class="w-fit px-5 py-1 my-2 ml-2 font-bold bg-accent_primary text-bg_primary rounded-full hover:shadow-lg hover:shadow-accent_secondary_transparent hover:bg-accent_secondary hover:rounded-lg transition transform duration-500" onclick="window.open('', '_blank')"><i class="text-bg_primary fa-brands fa-telegram"></i> Telegram</button>
