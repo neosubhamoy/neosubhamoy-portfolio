a518232f4709f381104689eb43124d1353e8ef80
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2024-01-08 00:04:22 +0530

    (refactor): changed smtp to tls

--- htdocs/core/handle_contact.php
+++ htdocs/core/handle_contact.php
@@ -35,7 +35,7 @@ function send_contact_email($name, $email, $message) {
         $mail->SMTPAuth   = true;
         $mail->Username   = $_ENV['SMTP_USER'];
         $mail->Password   = $_ENV['SMTP_PASS'];
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->SMTPSecure = "tls";
         $mail->Port       = $_ENV['SMTP_PORT'];
     
         $mail->setFrom($_ENV['SMTP_USER'], 'contact@neosubhamoy.com');
