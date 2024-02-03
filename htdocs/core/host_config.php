9b84a72b2d926090dc46972f935ba7628df5cbea
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2024-02-03 20:31:27 +0530

    (refactor): added root domain in host_config

--- htdocs/core/host_config.php
+++ htdocs/core/host_config.php
@@ -1,6 +1,7 @@
 <?php
 $currentHost = $_SERVER['HTTP_HOST'];
 $localIP = $_ENV['LOCAL_IP'];
+$domain = $_ENV['DOMAIN'];
 
 if($currentHost == "localhost") {
     $basePath = "https://localhost/neosubhamoy/htdocs";
