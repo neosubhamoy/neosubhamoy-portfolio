606f138042e84eeed54eda84440e42247d2a249c
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2024-07-29 23:05:55 +0530

    (refactor): updated dmca page

--- htdocs/dmca.php
+++ htdocs/dmca.php
@@ -31,27 +31,42 @@ write_dataset($conn);
                 </span>
                 <span class="px-4 py-1 bg-accent_primary_extratransparent rounded-full text-accent_primary hidden lg:block">#legal</span>
             </div>
-            <p class="text-lg text-accent_three mt-12">Last Updated On: 11th of December 2023</p>
+            <p class="text-lg text-accent_three mt-12">Last Updated On: 29th of July 2024</p>
             <div class="dmcacontent w-full bg-bg_secondary rounded-xl px-5 py-7 mt-7">
                 <div class="dmcacont1 text-accent_three pb-5 mb-10 border-b-[1px] border-b-accent_secondary_transparent">
                     <p>If you require any more information or have any questions regarding this site&#39;s disclaimer, please feel free to contact us by email at <a href="mailto:hey@<?php echo $domain ?>">hey@<?php echo $domain ?></a>.</p>
                 </div>
                 <div class="dmcacont2 text-accent_three">
                     <h2 class="text-2xl text-accent_secondary my-3">Disclaimers for Subhamoy Biswas Portfolio</h2>
-    
                     <p>All the information on this website - https://<?php echo $domain ?> - is published in good faith and for general information purpose only. Subhamoy Biswas Portfolio does not make any warranties about the completeness, reliability and accuracy of this information. Any action you take upon the information you find on this website (Subhamoy Biswas Portfolio), is strictly at your own risk. Subhamoy Biswas Portfolio will not be liable for any losses and/or damages in connection with the use of our website.</p>
-    
                     <p>From our website, you can visit other websites by following hyperlinks to such external sites. While we strive to provide only quality links to useful and ethical websites, we have no control over the content and nature of these sites. These links to other websites do not imply a recommendation for all the content found on these sites. Site owners and content may change without notice and may occur before we have the opportunity to remove a link which may have gone &#39;bad&#39;.</p>
-    
                     <p>Please be also aware that when you leave our website, other sites may have different privacy policies and terms which are beyond our control. Please be sure to check the Privacy Policies of these sites as well as their &quot;Terms of Service&quot; before engaging in any business or uploading any information.</p>
-    
-                    <h2 class="text-2xl text-accent_secondary my-3">Consent</h2>
-    
-                    <p>By using our website, you hereby consent to our disclaimer and agree to its terms.</p>
-    
-                    <h2 class="text-2xl text-accent_secondary my-3">Update</h2>
-    
-                    <p>Should we update, amend or make any changes to this document, those changes will be prominently posted here.</p>
+
+                    <h2 class="text-2xl text-accent_secondary my-3">What is open sourced and what is not</h2>
+                    <p>This website is fully open source (FOSS) and licensed under the <a href="https://opensource.org/license/mit">MIT License</a> but still some parts of this website are not allowed to use or distribute without proper permission or attribution. All the contents (eg: informations, blog posts, articles) published in this website and the visual components (eg: layout, design, animations, images, videos, graphics) used in this website are not covered under the <a href="https://opensource.org/license/mit">MIT License</a> and requires special permission to be used. Using these without prior permission will cause legal actions.</p>
+
+                    <h2 class="text-2xl text-accent_secondary my-3">DMCA Policy</h2>
+                    <p>This Digital Millennium Copyright Act policy (“Policy”) applies to the <a href="https://<?php echo $domain ?>">neosubhamoy.com</a> website (“Website” or “Service”) and any of its related products and services (collectively, “Services”) and outlines how this Website operator (“Operator”, “we”, “us” or “our”) addresses copyright infringement notifications and how you (“you” or “your”) may submit a copyright infringement complaint.</p>
+                    <p>Protection of intellectual property is of utmost importance to us and we ask our users and their authorized agents to do the same. It is our policy to expeditiously respond to clear notifications of alleged copyright infringement that comply with the United States Digital Millennium Copyright Act (“DMCA”) of 1998, the text of which can be found at the U.S. Copyright Office <a href="https://www.copyright.gov">website</a>.</p>
+
+                    <h2 class="text-2xl text-accent_secondary my-3">What to consider before submitting a copyright complaint</h2>
+                    <p>Please note that if you are unsure whether the material you are reporting is in fact infringing, you may wish to contact an attorney before filing a notification with us.</p>
+                    <p>The DMCA requires you to provide your personal information in the copyright infringement notification. If you are concerned about the privacy of your personal information, you may wish to <a href="https://www.copyrighted.com/professional-takedowns" ref="nofollow noreferrer noopener external">hire an agent</a> to report infringing material for you.</p>
+                    
+                    <h2 class="text-2xl text-accent_secondary my-3">Notifications of infringement</h2>
+                    <p>If you are a copyright owner or an agent thereof, and you believe that any material available on our Services infringes your copyrights, then you may submit a written copyright infringement notification (“Notification”) using the contact details below pursuant to the DMCA. All such Notifications must comply with the DMCA requirements.</p>
+                    <p>Filing a DMCA complaint is the start of a pre-defined legal process. Your complaint will be reviewed for accuracy, validity, and completeness. If your complaint has satisfied these requirements, our response may include the removal or restriction of access to allegedly infringing material.</p>
+                    <p>If we remove or restrict access to materials or terminate an account in response to a Notification of alleged infringement, we will make a good faith effort to contact the affected user with information concerning the removal or restriction of access.</p>
+                    <p>Notwithstanding anything to the contrary contained in any portion of this Policy, the Operator reserves the right to take no action upon receipt of a DMCA copyright infringement notification if it fails to comply with all the requirements of the DMCA for such notifications.</p>
+                    <p>The process described in this Policy does not limit our ability to pursue any other remedies we may have to address suspected infringement.</p>
+                    
+                    <h2 class="text-2xl text-accent_secondary my-3">Changes and amendments</h2>
+                    <p>We reserve the right to modify this Policy or its terms related to the Website and Services at any time at our discretion. When we do, we will revise the updated date at the top of this page. We may also provide notice to you in other ways at our discretion, such as through the contact information you have provided.</p>
+                    <p>An updated version of this Policy will be effective immediately upon the posting of the revised Policy unless otherwise specified. Your continued use of the Website and Services after the effective date of the revised Policy (or such other act specified at that time) will constitute your consent to those changes.</p>
+                    
+                    <h2 class="text-2xl text-accent_secondary my-3">Reporting copyright infringement</h2>
+                    <p>If you would like to notify us of the infringing material or activity, we encourage you to contact us using the details below:</p>
+                    <p><a href="mailto:abuse@<?php echo $domain ?>">abuse@<?php echo $domain ?></a></p>
                 </div>
             </div>
         </div>
